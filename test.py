f3c6874069c516757c28742d99ec4b52008c091a
Stepan D <h3xp3ctb@gmail.com>
2023-07-17 18:13:28 +0300

    17.07.2023 work

--- test.py
+++ test.py
@@ -1,40 +1,32 @@
+from PyQt6.QtCore import Qt
 import sys
-
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import *
-import sys
-from PyQt5.QtWidgets import (QWidget, QHBoxLayout,
-    QLabel, QApplication)
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt6.QtWidgets import QWidget, QHBoxLayout, QLabel, QApplication
+from PyQt6.QtGui import QPixmap, QImage
 
 
 class Example(QWidget):
 
-    def __init__(self):
-        super().__init__()
+	def __init__(self):
+		super().__init__()
+		self.initUI()
 
-        self.initUI()
+	def initUI(self):
+		hbox = QHBoxLayout(self)
 
+		image = QImage(100, 100, QImage.Format.Format_RGB32)
+		image.fill(Qt.GlobalColor.white)
+		pixmap = QPixmap(image)
 
-    def initUI(self):
+		lbl = QLabel(self)
+		lbl.setPixmap(pixmap)
 
-        hbox = QHBoxLayout(self)
+		hbox.addWidget(lbl)
+		#self.setLayout(hbox)
 
-        image = QImage(100,100, QImage.Format.Format_RGB32)
-        image.fill(Qt.GlobalColor.white)
-        pixmap = QPixmap(image)
-
-        lbl = QLabel(self)
-        lbl.setPixmap(pixmap)
-
-        hbox.addWidget(lbl)
-        self.setLayout(hbox)
-
-        self.show()
+		self.show()
 
 
 if __name__ == '__main__':
-
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+	app = QApplication(sys.argv)
+	ex = Example()
+	sys.exit(app.exec())
