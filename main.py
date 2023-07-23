4cd7b5b25c72c8ac50ec1fc9cae282825779dc1f
Stepan D <h3xp3ctb@gmail.com>
2023-07-23 17:49:22 +0300

    23.07.2023 work

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sys
 
-import qimage2ndarray
+from PyQt6 import QtGui
 from PyQt6.QtCore import Qt, QPoint
 from PyQt6.QtGui import QImage, QPainter, QPen, QPixmap, QCursor, QColor
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from numpy import asarray
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,20 +14,35 @@ from sklearn.model_selection import train_test_split # разделение вы
 
 from ui_mainwindow import Ui_MainWindow
 
-def number():
+def qimage_to_ndarray(qimage):
+    image = qimage.convertToFormat(QtGui.QImage.Format.Format_RGB32)
+
+    width = image.width()
+    height = image.height()
+
+    ptr = image.constBits()
+    ptr.setsize(height * width * 4)
+    arr = np.frombuffer(ptr, np.uint8).reshape((height, width, 4))
+    return arr
+
+def number(image):
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
     x_train = x_train / 255
     x_test = x_test / 255
+    image = image / 255
 
-
+    #print(type(x_test[0]))
+    #print(type(image))
     plt.imshow(x_test[0], cmap=plt.cm.binary)
+    plt.show()
+    plt.imshow(image, cmap=plt.cm.binary)
     plt.show()
 
     y_train_cat = keras.utils.to_categorical(y_train, 10)
     y_test_cat = keras.utils.to_categorical(y_test, 10)
 
-    size_val = 10000
+    size_val = 100
     x_val_split = x_train[:size_val]
     y_val_split = y_train_cat[:size_val]
 
@@ -43,15 +57,19 @@ def number():
     x_train_split, x_val_split, y_train_split, y_val_split = train_test_split(x_train, y_train_cat, test_size=0.2)
 
     # model.fit(x_train, y_train_cat, batch_size=32, epochs=5, validation_split=0.2) # после каждых 32 изображений будут обновляться весовые коеф., 5 проходов, 80%/20% обучение/валидация (проверка что не идет переобучение выборки)
-    model.fit(x_train, y_train_cat, batch_size=32, epochs=1, validation_data=(x_val_split, y_val_split))
+    model.fit(x_train, y_train_cat, batch_size=16, epochs=1, validation_data=(x_val_split, y_val_split))
 
     model.evaluate(x_test, y_test_cat)  # проверка на тестовой выборке
 
-    n = 100
-    x = np.expand_dims(x_test[n], axis=0)  # что бы из 2х мерной матрицы сделать 3х мерную
+    x = np.expand_dims(x_test[0], axis=0)  # что бы из 2х мерной матрицы сделать 3х мерную
+    print(x)
+    x = np.expand_dims(image, axis=0)  # что бы из 2х мерной матрицы сделать 3х мерную
+    print(x)
     res = model.predict(x)  # можно подавать только 3х мерную матрицу (несколько изображений)
-    #plt.imshow(x_test[n], cmap=plt.cm.binary)
-    #plt.show()
+    print(res)
+    print(f'Цифра: {np.argmax(res)}')  # индекс максимального значения
+    plt.imshow(image, cmap=plt.cm.binary)
+    plt.show()
 
     pred = model.predict(x_test)
     pred = np.argmax(pred, axis=1)  # axis=1 во второй области списка (слой3)
@@ -72,7 +90,7 @@ class Window(QMainWindow):
         self.image.fill(color)
 
         self.drawings = False
-        self.brushSize = 7
+        self.brushSize = 15
         self.brushColor = Qt.GlobalColor.black
         self.lastPoint = QPoint()
 
@@ -115,9 +133,15 @@ class Window(QMainWindow):
     def recognize_picture(self):
         cropped_pixmap = self.image.copy(10, 193, 380, 380)
         cropped_pixmap = cropped_pixmap.scaled(28, 28)
-        numpydata = qimage2ndarray.recarray_view(cropped_pixmap)
-        #print(type(numpydata))
-        number()
+        # width = cropped_pixmap.width()
+        # height = cropped_pixmap.height()
+        # buffer = cropped_pixmap.bits().asstring(cropped_pixmap.byteCount())
+        # dtype = np.uint8 if cropped_pixmap.format() == QImage.Format.Format_Indexed8 else np.uint32
+        # ndarray = np.frombuffer(buffer, dtype=dtype).reshape((height, width))
+        # plt.imshow(ndarray, cmap='gray')  # Assuming it's a grayscale image
+        # plt.show()
+        numpydata = qimage_to_ndarray(cropped_pixmap)
+        number(numpydata)
 
 
 
