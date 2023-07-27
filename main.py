66f5e993f9065785cef95041e167fb7cb32c9f62
Stepan D <h3xp3ctb@gmail.com>
2023-07-27 23:42:38 +0300

    27.07.2023 home

--- main.py
+++ main.py
@@ -15,89 +15,91 @@ from ui_mainwindow import Ui_MainWindow
 
 
 class Window(QMainWindow):
-	
+
 	def __init__(self):
 		super().__init__()
 		self.ui = Ui_MainWindow()
 		self.ui.setupUi(self)
-		
+
 		self.image = QImage(self.size(), QImage.Format.Format_RGB32)
 		color = QColor(240, 240, 240)
 		self.image.fill(color)
-		
+
 		self.drawings = False
 		self.brushSize = 25
 		self.brushColor = Qt.GlobalColor.black
 		self.lastPoint = QPoint()
-		
+
 		self.ui.pushButton_recognize.clicked.connect(self.recognize_picture)
 		self.ui.pushButton_clear.clicked.connect(self.clearDisplay)
-		
+
 		self.neural_network = number.NN_number()
-		self.neural_network.predict()
-	
+
 	def mousePressEvent(self, event):
 		if event.button() == Qt.MouseButton.LeftButton and self.check_position():
 			self.drawings = True
 			self.lastPoint = event.position()
-	
+
 	def mouseMoveEvent(self, event):
 		if event.buttons() & Qt.MouseButton.LeftButton and self.check_position():
 			painter = QPainter(self.image)
 			painter.setPen(QPen(self.brushColor,
-			                    self.brushSize,
-			                    Qt.PenStyle.SolidLine,
-			                    Qt.PenCapStyle.RoundCap,
-			                    Qt.PenJoinStyle.RoundJoin))
+								self.brushSize,
+								Qt.PenStyle.SolidLine,
+								Qt.PenCapStyle.RoundCap,
+								Qt.PenJoinStyle.RoundJoin))
 			painter.drawLine(self.lastPoint, event.position())
 			painter.end()
 			self.lastPoint = event.position()
 			self.update()
-	
+
 	def mouseReleaseEvent(self, event):
 		if event.button() == Qt.MouseButton.LeftButton and self.check_position():
 			self.drawings = False
-	
+
 	def paintEvent(self, event):
 		canvasPainter = QPainter(self)
 		if self.check_position():
 			canvasPainter.drawImage(self.rect(), self.image, self.image.rect())
-	
+
 	def check_position(self) -> bool:
 		cursor_pos = QCursor.pos()
 		window_pos = self.mapFromGlobal(cursor_pos)
-		if (self.ui.groupBox.x() + (self.brushSize + 1) // 2) < window_pos.x() < (self.ui.groupBox.x() + self.ui.groupBox.width() - (self.brushSize + 1) // 2) and \
-			(self.ui.groupBox.y() + (self.brushSize + 1) // 2) < window_pos.y() < (self.ui.groupBox.y() + self.ui.groupBox.height() - (self.brushSize + 1) // 2):
+		if (self.ui.groupBox.x() + (self.brushSize + 1) // 2) < window_pos.x() < \
+			(self.ui.groupBox.x() + self.ui.groupBox.width() - (self.brushSize + 1) // 2) and \
+			(self.ui.groupBox.y() + (self.brushSize + 1) // 2) < window_pos.y() < \
+			(self.ui.groupBox.y() + self.ui.groupBox.height() - (self.brushSize + 1) // 2):
 			return True
 		else:
 			return False
-	
+
 	def replace_color(self, qimage, old_color, new_color):
 		image_array = qimage2ndarray.rgb_view(qimage)
 		mask = np.all(image_array == old_color, axis=-1)
 		image_array[mask] = new_color
 		new_qimage = qimage2ndarray.array2qimage(image_array)
 		return new_qimage
-	
+
 	def recognize_picture(self):
 		cropped_pixmap = self.image.copy(10, 193, 380, 380)
 		cropped_pixmap = self.replace_color(cropped_pixmap, [240, 240, 240], [255, 255, 255])
 		cropped_pixmap.save('image.png')
 		img_path = 'image.png'
-		
+
 		img = keras.preprocessing.image.load_img(img_path)
 		image = img.resize((28, 28), Image.BILINEAR)
 		image.save('image.png')
 		img = cv2.imread('image.png')
 		image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 		image = np.array(image)
-		self.neural_network.recognize_picture(image)
-	
+		self.setProgressBars(self.neural_network.recognize_picture(image))
+
 	def clearDisplay(self):
 		color = QColor(240, 240, 240)
 		self.image.fill(color)
 		self.update()
-	
+		self.setProgressBars([[0]*10])
+
 	def setProgressBars(self, arr):
 		self.ui.progressBar_0.setValue(int(arr[0][0] * 100))
 		self.ui.progressBar_1.setValue(int(arr[0][1] * 100))
--- number.py
+++ number.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-import main
+from main import *
 
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.datasets import mnist  # библиотека рукописных цифр
-from sklearn.model_selection import train_test_split  # разделение выборонов�
+from tensorflow.keras.layers import Dense, Flatten, MaxPool2D, Conv2D
+from tensorflow.keras.datasets import mnist
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import load_model
 
 
 class NN_number():
@@ -14,7 +15,7 @@ class NN_number():
 		self.model = None
 		self.x_test = None
 	
-	def predict(self):
+	def predict_nn(self):
 		(x_train, y_train), (self.x_test, y_test) = mnist.load_data()
 		
 		x_train = x_train / 255
@@ -24,21 +25,35 @@ class NN_number():
 		y_test_cat = keras.utils.to_categorical(y_test, 10)
 		
 		self.model = keras.Sequential([
-			Flatten(input_shape=(28, 28, 1)),
-			Dense(128, activation='relu'),
+			Conv2D(
+				input_shape=(28, 28, 1),
+				filters=32,
+				kernel_size=(5, 5),
+				padding='same',
+				activation='relu',
+			),
+			MaxPool2D(pool_size=(2, 2)),
+			Flatten(),
+			Dense(512, activation='relu'),
 			Dense(10, activation='softmax')
 		])
 		
-		self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+		self.model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 		
 		x_train_split, x_val_split, y_train_split, y_val_split = train_test_split(x_train, y_train_cat, test_size=0.2)
-		self.model.fit(x_train, y_train_cat, batch_size=32, epochs=5, validation_data=(x_val_split, y_val_split))
+		self.model.fit(x_train.reshape(-1, 28, 28, 1), y_train, epochs=5)
 		
-		self.model.evaluate(self.x_test, y_test_cat)  # проверка на тестовой выборке
+		#self.model.evaluate(self.x_test, y_test_cat)  # проверка на тестовой выборке
 	
 	def recognize_picture(self, image):
 		image = image / 255
 		image = 1 - image
 		x = np.expand_dims(image, axis=0)  # что бы из 2х мерной матрицы сделать 3х мерную
-		res = self.model.predict(x)  # можно подавать только 3х мерную матрицу (несколько изображений)
-		main.setProgressBars(res)
+		model = load_model('nn_recognize_number.keras')
+		res = model.predict(x)  # можно подавать только 3х мерную матрицу (несколько изображений)
+		return res
+	
+if __name__ == '__main__':
+	neural = NN_number()
+	neural.predict_nn()
+	neural.model.save('nn_recognize_number.keras')
