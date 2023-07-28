73032bfc5a5c89b6e60e615bb54c021fbe81c387
Stepan D <h3xp3ctb@gmail.com>
2023-07-28 17:04:02 +0300

    28.07.2023 work

--- number.py
+++ number.py
@@ -3,9 +3,8 @@ import numpy as np
 from main import *
 
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Flatten, MaxPool2D, Conv2D
+from tensorflow.keras.layers import Dense, Flatten, MaxPool2D, Conv2D, Dropout
 from tensorflow.keras.datasets import mnist
-from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import load_model
 
 
@@ -21,9 +20,6 @@ class NN_number():
 		x_train = x_train / 255
 		self.x_test = self.x_test / 255
 		
-		y_train_cat = keras.utils.to_categorical(y_train, 10)
-		y_test_cat = keras.utils.to_categorical(y_test, 10)
-		
 		self.model = keras.Sequential([
 			Conv2D(
 				input_shape=(28, 28, 1),
@@ -33,17 +29,24 @@ class NN_number():
 				activation='relu',
 			),
 			MaxPool2D(pool_size=(2, 2)),
+			Conv2D(
+				filters=64,
+				kernel_size=(5, 5),
+				padding='same',
+				activation='relu',
+			),
+			MaxPool2D(pool_size=(2, 2)),
 			Flatten(),
-			Dense(512, activation='relu'),
+			Dense(1024, activation='relu'),
+			Dropout(0.4),
 			Dense(10, activation='softmax')
 		])
 		
 		self.model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 		
-		x_train_split, x_val_split, y_train_split, y_val_split = train_test_split(x_train, y_train_cat, test_size=0.2)
 		self.model.fit(x_train.reshape(-1, 28, 28, 1), y_train, epochs=5)
 		
-		#self.model.evaluate(self.x_test, y_test_cat)  # проверка на тестовой выборке
+		self.model.evaluate(self.x_test.reshape(-1, 28, 28, 1), y_test)
 	
 	def recognize_picture(self, image):
 		image = image / 255
